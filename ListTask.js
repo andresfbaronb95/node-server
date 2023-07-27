f6b24f65bd32b45954b1b9f820ce44e1c93330c8
Andres Felipe Baron <pipe5477@gmail.com>
2023-07-26 19:56:33 -0500

    Agregando el código del servidor de lista de tareas en la rama principal

--- ListTask.js
+++ ListTask.js
@@ -1,122 +1,43 @@
-const readline = require('readline');
+const http = require('http');
 
 const tasks = [];
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-function showMenu() {
-  console.log('===== Lista de Tareas =====');
-  console.log('1. Ver tareas');
-  console.log('2. Añadir tarea');
-  console.log('3. Completar tarea');
-  console.log('4. Eliminar tarea');
-  console.log('0. Salir');
-}
-
-function showTasks() {
-  console.log('===== Tareas =====');
-  tasks.forEach((task, index) => {
-    console.log(`${index + 1}. [${task.completed ? 'X' : ' '}] ${task.description}`);
-  });
-}
-
 function addTask(description) {
-  return new Promise((resolve, reject) => {
-    tasks.push({ description, completed: false });
-    resolve('Tarea añadida.');
-  });
+  tasks.push({ description, completed: false });
+  return 'Tarea añadida.';
 }
 
 function completeTask(index) {
-  return new Promise((resolve, reject) => {
-    if (index >= 0 && index < tasks.length) {
-      tasks[index].completed = true;
-      resolve('Tarea completada.');
-    } else {
-      reject('Índice de tarea inválido.');
-    }
-  });
-}
-
-function deleteTask(index) {
-  return new Promise((resolve, reject) => {
-    if (index >= 0 && index < tasks.length) {
-      tasks.splice(index, 1);
-      resolve('Tarea eliminada.');
-    } else {
-      reject('Índice de tarea inválido.');
-    }
-  });
-}
-
-async function handleInput(input) {
-  switch (input) {
-    case '1':
-      showTasks();
-      break;
-    case '2':
-      const description = await new Promise((resolve) => {
-        rl.question('Descripción de la tarea: ', (description) => {
-          resolve(description);
-        });
-      });
-      try {
-        const result = await addTask(description);
-        console.log(result);
-        showMenu();
-      } catch (error) {
-        console.error(error);
-      }
-      break;
-    case '3':
-      const completeIndex = await new Promise((resolve) => {
-        rl.question('Índice de la tarea a completar: ', (index) => {
-          resolve(parseInt(index) - 1);
-        });
-      });
-      try {
-        const result = await completeTask(completeIndex);
-        console.log(result);
-        showMenu();
-      } catch (error) {
-        console.error(error);
-      }
-      break;
-    case '4':
-      const deleteIndex = await new Promise((resolve) => {
-        rl.question('Índice de la tarea a eliminar: ', (index) => {
-          resolve(parseInt(index) - 1);
-        });
-      });
-      try {
-        const result = await deleteTask(deleteIndex);
-        console.log(result);
-        showMenu();
-      } catch (error) {
-        console.error(error);
-      }
-      break;
-    case '0':
-      rl.close();
-      break;
-    default:
-      console.log('Opción inválida. Introduce una opción válida.');
-      showMenu();
+  if (index >= 0 && index < tasks.length) {
+    tasks[index].completed = true;
+    return 'Tarea completada.';
+  } else {
+    throw new Error('Índice de tarea inválido.');
   }
 }
 
-function handleInputThen(input) {
-  handleInput(input)
-    .then(() => {})
-    .catch((error) => {
-      console.log(error);
-    });
+function deleteTask(index) {
+  if (index >= 0 && index < tasks.length) {
+    tasks.splice(index, 1);
+    return 'Tarea eliminada.';
+  } else {
+    throw new Error('Índice de tarea inválido.');
+  }
 }
 
-showMenu();
-rl.on('line', (input) => {
-  handleInputThen(input);
+const server = http.createServer((req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  if (url.pathname === '/tasks' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(tasks));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 });
+
+server.listen(8000, 'localhost', () => {
+  console.log('Servidor escuchando en http://localhost:8000');
+});
+
